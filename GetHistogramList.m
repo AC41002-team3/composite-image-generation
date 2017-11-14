252fb0bdc93f0a8f65753b750b539abe5c705a32
ijm7 <ijmorton500@gmail.com>
2017-11-14 11:42:23 +0000

    Revised LoadImages.m to return a cell array

---
+++ GetHistogramList.m
@@ -0,0 +1,8 @@
+function [ output_args ] = GetHistogramList( input_args )
+%GETHISTOGRAMLIST Summary of this function goes here
+    images = LoadImages('./');
+    
+    
+    
+end
+
--- LoadImages.m
+++ LoadImages.m
@@ -1,6 +1,12 @@
 function [ imageList ] = LoadImages( imageDirectory )
-% LOADIMAGES Provide directory of images,
-% and a structured array shall be returned.
-    imageList = dir(strcat(imageDirectory, '*.jpg'));
+%LOADIMAGES Provide directory of images,
+%           and a structured array shall be returned.
+    rawImages = dir(strcat(imageDirectory, '*.jpg'));
+    nFiles = length(rawImages);
+    for i = 1:nFiles
+        currentFileName = rawImages(i).name;
+        currentRawImage = imread(currentFileName);
+        imageList{i} = currentRawImage;
+    end
 end
 
