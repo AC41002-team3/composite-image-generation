53fe6a93b56c47397ba575204d757f5ec96984f3
zhaks <danielzhaks@gmail.com>
2017-11-17 15:06:26 +0000

    General bug fixing - getting closer

--- RunApp.m
+++ RunApp.m
@@ -1,8 +1,8 @@
 function cIm = RunApp (src,tgt,tileN)
-    [tRGB,x,y,xT,yT] = TileImage(src,tileN);
+    [tRGB,x,y,xT,yT] = TileImage(tgt,tileN);
     srcR = ResizeSourceImages(src,x,y);
     srcRGB=GetSrcRGBs(srcR);
     matches=FindTiles(srcRGB,tRGB);
     cIm=CreateImage(srcR,matches,xT,yT);
-    
+    imshowpair(tgt,cIm,'montage');
 end
--- TileImage.m
+++ TileImage.m
@@ -1,12 +1,12 @@
 %%Get number of tiles and the target RGB values for them
-function [tileRGB,xTLen,yTLen,xTiles,yTiles] = TileImage (src, tileN)
+function [tileRGB,xTLen,yTLen,xTiles,yTiles] = TileImage (tgt, tileN)
     %%Get the number of x and y pixels
-    [xpx,ypx] = size(src);
+    [xpx,ypx,zpx] = size(tgt);
     %%
     %%Caluclate X and Y tile numbers||DOES NOT WORK WITH SQUARES
     dvsrs = divisor(tileN);
-    xTiles = dvsrs(length(dvsrs)/2);
-    yTiles = dvsrs((length(dvsrs)/2+1));
+    xTiles = dvsrs(floor((length(dvsrs)/2)));
+    yTiles = dvsrs(floor((length(dvsrs)/2)+1));
     %%
     %%Calculate tile widths and heights
     xTLen = round(xpx/xTiles);
@@ -14,13 +14,13 @@ function [tileRGB,xTLen,yTLen,xTiles,yTiles] = TileImage (src, tileN)
     %%
     %%Calculate RGB values of each tile
     tRGB=cell(xTLen,yTLen);
-        parfor i=1:xTiles
-            c=(i*xTiles)-xTiles;
+        for i=1:xTiles
+            c=(i*xTLen);
             for j=1:yTiles                         
-                r=(j*yTiles)-yTiles;
-                srcTl=imcrop(src,[c r (c+xTiles) (r+yTiles)]);
+                r=(j*yTLen);
+                srcTl=imcrop(tgt,[c r (c+xTLen) (r+yTLen)]);
                 tRGB{i,j}= {mean(mean(srcTl))};                
             end            
         end
-    tileRGB=tRGB(~cellfun('isempty',tRGB));
+    tileRGB=tRGB(~cellfun('isempty',tRGB))';
 end
