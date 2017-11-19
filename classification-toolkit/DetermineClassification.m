65dbf116c51318a3493e80f3f391909ac1563c09
ijm7 <ijmorton500@gmail.com>
2017-11-19 16:07:38 +0000

    modifications for part-b for new file structure

--- classification-toolkit/DetermineClassification.m
+++
@@ -1,7 +0,0 @@
-function [ output_args ] = DetermineClassification( trainingHist, testHist )
-%DETERMINECLASSIFICATION Summary of this function goes here
-%   Detailed explanation goes here
-
-
-end
-
--- classification-toolkit/RunTestSet.m
+++ classification-toolkit/RunTestSet.m
@@ -1,7 +1,7 @@
 function [ testManmade, testNatural ] = RunTestSet()
 %RUNTESTSET Summary of this function goes here
 %   Detailed explanation goes here
-    testManmade = GetHistogramList('../Assignments/Images/out_manmade_test_1k/');
-    testNatural = GetHistogramList('../Assignments/Images/out_natural_test_1k/');
+    testManmade = GetHistogramList('../../Assignments/Images/out_manmade_test_1k/');
+    testNatural = GetHistogramList('../../Assignments/Images/out_natural_test_1k/');
 end
 
