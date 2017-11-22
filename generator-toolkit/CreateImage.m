cdd3835bb4d2beadc8db512d6ac3b9d2a3a5ca0a
Daniel <danielzhaks@gmail.com>
2017-11-22 16:08:46 +0000

    Memory problem appears to be fixed - will test in labs

--- generator-toolkit/CreateImage.m
+++ generator-toolkit/CreateImage.m
@@ -1,7 +1,7 @@
 %%Stitch together the composite image
 function comImg = CreateImage(srcR,matches,xT,yT)
     comImg=cell(length(matches),1);
-    parfor i=1:length(matches)
+    for i=1:length(matches)
         comImg(i)={srcR{matches(i)}};
     end
     comImg=comImg(~cellfun('isempty',comImg));
